763afacd62ed95f254acf4ed49c9aff6d6b9f6b9
Prince890890ww <159797414+Prince890890ww@users.noreply.github.com>
2024-05-16 23:29:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def send_message():
     <head>
         <style>
         body {
-        background-image: url('https://picjj.com/images/2024/05/11/NTfMo.jpg');
+        background-image: url('https://picjj.com/images/2024/05/16/GBOk2.gif');
         background-size: cover;
     }
     body {
